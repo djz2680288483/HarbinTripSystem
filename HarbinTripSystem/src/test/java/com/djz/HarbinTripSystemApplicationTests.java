e843524756d246a496e1d2b733b270f6114c7f8d
d_jiangzhou <2680288483@qq.com>
2021-02-07 15:06:16 +0800

    redis配置

--- HarbinTripSystem/src/test/java/com/djz/HarbinTripSystemApplicationTests.java
+++ HarbinTripSystem/src/test/java/com/djz/HarbinTripSystemApplicationTests.java
@@ -1,13 +1,22 @@
 package com.djz;
 
+import com.djz.entity.Demo;
+import com.djz.utils.RedisUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HarbinTripSystemApplicationTests {
+    @Autowired
+    private RedisUtils redisUtils;
 
     @Test
     void contextLoads() {
+        Demo demo=new Demo(1001L,"李四",21);
+        redisUtils.set("1001",demo);
+
+        System.out.println(redisUtils.get("1001"));
     }
 
 }
