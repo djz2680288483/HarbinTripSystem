427be7227cd1b3fb6ab04d8a3ce475edcc742fb6
d_jiangzhou <2680288483@qq.com>
2021-02-16 00:56:21 +0800

    test

--- HarbinTripSystem/src/test/java/com/djz/HarbinTripSystemApplicationTests.java
+++ HarbinTripSystem/src/test/java/com/djz/HarbinTripSystemApplicationTests.java
@@ -1,6 +1,8 @@
 package com.djz;
 
 import com.djz.entity.Demo;
+import com.djz.entity.User;
+import com.djz.mapper.UserMapper;
 import com.djz.utils.RedisUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -18,16 +20,26 @@ class HarbinTripSystemApplicationTests {
     @Autowired
     AmqpTemplate amqpTemplate;
 
+    @Autowired
+    private UserMapper userMapper;
+
     void contextLoads() {
-        Demo demo=new Demo(1001L,"李四",21);
-        redisUtils.set("1001",demo);
+        Demo demo = new Demo(1001L, "李四", 21);
+        redisUtils.set("1001", demo);
 
         System.out.println(redisUtils.get("1001"));
     }
 
     @Test
     void contextLoads2() {
-
+        User user = new User();
+        user.setId(3L);
+        user.setName("admin");
+        user.setPassword("admin");
+        user.setQq("2680288483");
+        user.setTelephone("15186001530");
+      int b=  userMapper.updateById(user);
+        System.out.println("==========="+b+"===========");
 
     }
 
