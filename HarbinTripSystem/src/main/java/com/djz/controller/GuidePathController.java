8927c1d231fc9e6ff41e25c01b0e98e759284da3
d_jiangzhou <2680288483@qq.com>
2021-02-17 22:33:39 +0800

    test

--- HarbinTripSystem/src/main/java/com/djz/controller/GuidePathController.java
+++ HarbinTripSystem/src/main/java/com/djz/controller/GuidePathController.java
@@ -1,7 +1,17 @@
 package com.djz.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.djz.entity.GuidePath;
+import com.djz.service.IGuidePathService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author djz
@@ -10,4 +20,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/guide/")
 public class GuidePathController {
+
+    @Autowired
+    private IGuidePathService guidePathService;
+
+    @PostMapping("/list")
+    @ApiOperation(value = "查询导航记录")
+    public String pageGuidePath(Long userId, ModelMap modelMap) {
+
+        List<GuidePath> list=guidePathService.getListByUserId(userId);
+
+        modelMap.addAttribute("list",list);
+        return "list";
+    }
+
+
 }
